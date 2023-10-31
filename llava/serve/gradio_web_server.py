1afdf062f72c5486f973348cbd9ee5fe2ff94f89
Ricardo Moran <144283575+AI4Ric@users.noreply.github.com>
2023-10-31 14:13:36 +0000

    Update gradio_web_server.py
    New line 246 to view the json response

--- llava/serve/gradio_web_server.py
+++ llava/serve/gradio_web_server.py
@@ -243,6 +243,7 @@ def http_bot(state, model_selector, temperature, top_p, max_new_tokens, request:
         response = requests.post(worker_addr + "/worker_generate_stream",
             headers=headers, json=pload, stream=False, timeout=10)  
         if response.status_code == 200:
+            print(response.text)
             data = json.loads(response.text)
             if data["error_code"] == 0:
                 output = data["text"][len(prompt):].strip()
