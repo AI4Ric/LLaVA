0b44991349de30f32054d235d98bb84d5d742fb0
Ricardo Moran <144283575+AI4Ric@users.noreply.github.com>
2023-11-02 14:16:42 +0000

    Update Nrun_command.sh
    new version

--- Nrun_command.sh
+++ Nrun_command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm response.txt
-curl -X POST http://localhost:40000/worker_generate_stream -H "Content-Type: application/json" -d '{"model":"llava-v1.5-13b","prompt":"A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human\'s questions. USER: hello, how are you today? ASSISTANT:","temperature":0.2,"top_p":0.7,"max_new_tokens":512,"stop":"</s>","images":[]}' --output response.txt
+rm -f response.txt
+curl -X POST http://localhost:40000/worker_generate_stream -H "Content-Type: application/json" -d '{"model":"llava-v1.5-13b","prompt":"A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human'\''s questions. USER: hello, how are you today? ASSISTANT:","temperature":0.2,"top_p":0.7,"max_new_tokens":512,"stop":"</s>","images":[]}' --output response.txt
